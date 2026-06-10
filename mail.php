36941e01840c1f89aa1dadf6d50d28b34d925bfb
RicardoCordo <105396920+RicardoCordo@users.noreply.github.com>
2026-06-10 18:26:42 -0300

    arreglo formulario

--- mail.php
+++ mail.php
@@ -1,45 +1,48 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-if ($_POST)	{
 
-    $name = $_POST['name'];
-	$from = $_POST['email'];
-	$phone = $_POST['phone'];
-	$message = $_POST['message'];
+if ($_POST) {
 
+    $name    = $_POST['name'];
+    $from    = $_POST['email'];
+    $phone   = $_POST['phone'];
+    $message = $_POST['message'];
 
+    $to      = "jodearrefrigeracion@gmail.com";
+    $subject = "Nueva consulta desde jodear.com.ar";
 
-	// set response code - 200 OK
+    $msg = "
+    <html>
+    <body>
+        <h2>Nueva consulta desde la web</h2>
 
-	http_response_code(200);
-	$subject = "Nuevo mensaje de formulario de contacto.";
-	$to = "jodearrefrigeracion@gmail.com";
+        <p><strong>Nombre:</strong> {$name}</p>
+        <p><strong>Email:</strong> {$from}</p>
+        <p><strong>Teléfono:</strong> {$phone}</p>
 
-	// data
+        <p><strong>Mensaje:</strong></p>
+        <p>{$message}</p>
+    </body>
+    </html>
+    ";
 
-	$msg = $name . ' escribió: ' . $message;
+    $headers  = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
 
-	// Headers
+    // Remitente REAL de tu dominio
+    $headers .= "From: Jodear Refrigeración <info@jodear.com.ar>\r\n";
 
-	$headers = "MIME-Version: 1.0\r\n";
-	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers.= "From: <" . $from . ">";
-	
-	$result = mail($to, $subject, $msg, $headers);
+    // Cuando respondas el mail, responderá al cliente
+    $headers .= "Reply-To: {$from}\r\n";
 
+    $result = mail($to, $subject, $msg, $headers);
 
-    if($result) {
+    if ($result) {
         header('Location: https://jodear.com.ar/gracias');
+        exit;
+    } else {
+        echo "Error al enviar el correo.";
     }
-      else
-	{
-
-    header('Location: http://jodear.com.ar/gracias');
-	
-	}
-
 }
-	
-
 ?>
